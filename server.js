83f9f4da6caa981229414cb91e6fe4fa5f796cd1
Timeness <careless@flash.co>
2025-02-15 12:31:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,14 +23,17 @@ io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
     socket.on('offer', (data) => {
+        console.log('Offer received:', data);
         socket.broadcast.emit('offer', data);
     });
 
     socket.on('answer', (data) => {
+        console.log('Answer received:', data);
         socket.broadcast.emit('answer', data);
     });
 
     socket.on('ice-candidate', (data) => {
+        console.log('ICE Candidate received:', data);
         socket.broadcast.emit('ice-candidate', data);
     });
 
