626ceb9fc83cb26548e54aa53bd630bc681ad6ff
Timeness <careless@flash.co>
2025-02-15 12:59:28 +0530

    done

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const io = require('socket.io')(http, {
     }
 });
 
-// Static files
+//
 app.use(express.static('public'));
 
-// Routes
+//
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/viewer.html');
 });
@@ -18,13 +18,13 @@ app.get('/broadcast', (req, res) => {
     res.sendFile(__dirname + '/public/broadcaster.html');
 });
 
-// Socket.io
+//
 io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
-    socket.on('image', (data) => {
-        // Forward the image to all viewers
-        socket.broadcast.emit('image', data);
+    socket.on('selfie', (data) => {
+        //
+        socket.broadcast.emit('selfie', data);
     });
 
     socket.on('disconnect', () => {
@@ -32,7 +32,7 @@ io.on('connection', (socket) => {
     });
 });
 
-// Start server
+//
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
